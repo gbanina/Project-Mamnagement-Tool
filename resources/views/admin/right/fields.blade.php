8af3fe0d5ce2bc2582ddf43251631186605c667e
Goran Banina <gbanina@gmail.com>
2017-03-31 22:25:03 +0200

    project rights view refactoring

---
+++ resources/views/admin/right/fields.blade.php
@@ -0,0 +1,48 @@
+@foreach($project->projectType->posibleTaskTypes as $taskType)
+    @if(count($taskType->fields) != 0)
+<table class="table table-striped">
+    <thead>
+        <tr>
+            <th style="border-bottom:none; border-top: none;">{{$taskType->name}}</th>
+        </tr>
+    </thead>
+    <tbody class="hidden-project-right-fields2">
+                          @foreach($taskType->fields as $field)
+                          <tr>
+                            <td style="width: 220px">{{$field->label}}</td>
+
+                              @foreach($roles as $role)
+                                <td>
+                                @if(empty($field_rights[$project->id][$role->id][$taskType->id][$field->id]))
+                                  <div class="btn-group right_{{$project->id}}_{{$role->id}}" data-toggle="buttons">
+                                    <label class="btn btn-default active">
+                                      <input type="radio" name="field_right[{{$project->id}}][{{$role->id}}][{{$taskType->id}}][{{$field->id}}]" value="NONE" checked> 0
+                                    </label>
+                                    <label class="btn btn-default">
+                                      <input type="radio" name="field_right[{{$project->id}}][{{$role->id}}][{{$taskType->id}}][{{$field->id}}]" value="READ"> R
+                                    </label>
+                                    <label class="btn btn-default">
+                                      <input type="radio" name="field_right[{{$project->id}}][{{$role->id}}][{{$taskType->id}}][{{$field->id}}]" value="WRITE"> W
+                                    </label>
+                                  </div>
+                                @else
+                                  <div class="btn-group right_{{$project->id}}_{{$role->id}}" data-toggle="buttons">
+                                    <label class="btn btn-default @if($field_rights[$project->id][$role->id][$taskType->id][$field->id] == 'NONE') active @endif">
+                                      <input type="radio" name="field_right[{{$project->id}}][{{$role->id}}][{{$taskType->id}}][{{$field->id}}]" value="NONE" @if($field_rights[$project->id][$role->id][$taskType->id][$field->id] == 'NONE') checked @endif> 0
+                                    </label>
+                                    <label class="btn btn-default @if($field_rights[$project->id][$role->id][$taskType->id][$field->id] == 'READ') active @endif" @if($field_rights[$project->id][$role->id][$taskType->id][$field->id] == 'READ') checked @endif>
+                                      <input type="radio" name="field_right[{{$project->id}}][{{$role->id}}][{{$taskType->id}}][{{$field->id}}]" value="READ"> R
+                                    </label>
+                                    <label class="btn btn-default @if($field_rights[$project->id][$role->id][$taskType->id][$field->id] == 'WRITE') active @endif" @if($field_rights[$project->id][$role->id][$taskType->id][$field->id] == 'WRITE') checked @endif>
+                                      <input type="radio" name="field_right[{{$project->id}}][{{$role->id}}][{{$taskType->id}}][{{$field->id}}]" value="WRITE"> W
+                                    </label>
+                                  </div>
+                                @endif
+                                </td>
+                              @endforeach <!-- role -->
+                          </tr>
+                          @endforeach <!-- field -->
+    </tbody>
+</table>
+    @endif
+@endforeach <!-- task type -->
--- resources/views/admin/right/index.blade.php
+++ resources/views/admin/right/index.blade.php
@@ -2,30 +2,31 @@
 
 @section('content')
 
-<div class="">
+
         <div class="clearfix"></div>
             <div class="row">
               <div class="col-md-12" style="{{$viewStyle}}">
-                <div class="x_panel">
-                  <div class="x_title">
-                    <h2>Rights <small><a href="{{ URL::to('project') }}">Projects</a> | Project Rights</small></h2>
-                    <div class="clearfix"></div>
-                  </div>
-                  <div class="x_content" style="display: block;">
                     {!! Form::open(array('url' => 'admin/right')) !!}
                       {!! Form::submit('Save', array('class' => 'btn btn-success')) !!}
                       @foreach($projects as $project)
-                      <h4><strong>{{$project->name}}</strong></h4>
-                      <table class="table table-striped">
+<div class="x_panel" style="height: auto;">
+      <div class="x_title">
+              <table class="table table-striped">
                         <thead>
                           <tr>
-                              <th style="width: 180px;"></th>
+                              <th style="width: 220px">
+                                <h5 style=" white-space: nowrap;">
+                                  <a class="collapse-link">{{$project->name}}
+                                  <i class="fa fa-chevron-up"></i></a>
+                                </h5>
+
+                              </th>
                             @foreach($roles as $role)
                               <th>{{$role->name}} <a href="{{ URL::to('morph/' . $role->id) }}"><i class="fa fa-eye"></i></a></th>
                             @endforeach
                           </tr>
                            <tr>
-                              <th style="width: 180px;"></th>
+                              <th></th>
                             @foreach($roles as $role)
                               <th>
                                 @if(empty($field_rights[$project->id]))
@@ -63,68 +64,27 @@
                             @endforeach
                           </tr>
                         </thead>
-                      @foreach($project->projectType->posibleTaskTypes as $taskType)
-                        <thead>
-                          <tr>
-                            <th style="border-bottom:none; border-top: none;">{{$taskType->name}}</th>
-                          </tr>
-                        </thead>
-                        <tbody>
-                        </tbody>
-                        <tbody>
-                          @foreach($taskType->fields as $field)
-                          <tr>
-                            <td>{{$field->label}}</td>
-
-                              @foreach($roles as $role)
-                                <td>
-                                @if(empty($field_rights[$project->id][$role->id][$taskType->id][$field->id]))
-                                  <div class="btn-group right_{{$project->id}}_{{$role->id}}" data-toggle="buttons">
-                                    <label class="btn btn-default active">
-                                      <input type="radio" name="field_right[{{$project->id}}][{{$role->id}}][{{$taskType->id}}][{{$field->id}}]" value="NONE" checked> 0
-                                    </label>
-                                    <label class="btn btn-default">
-                                      <input type="radio" name="field_right[{{$project->id}}][{{$role->id}}][{{$taskType->id}}][{{$field->id}}]" value="READ"> R
-                                    </label>
-                                    <label class="btn btn-default">
-                                      <input type="radio" name="field_right[{{$project->id}}][{{$role->id}}][{{$taskType->id}}][{{$field->id}}]" value="WRITE"> W
-                                    </label>
-                                  </div>
-                                @else
-                                  <div class="btn-group right_{{$project->id}}_{{$role->id}}" data-toggle="buttons">
-                                    <label class="btn btn-default @if($field_rights[$project->id][$role->id][$taskType->id][$field->id] == 'NONE') active @endif">
-                                      <input type="radio" name="field_right[{{$project->id}}][{{$role->id}}][{{$taskType->id}}][{{$field->id}}]" value="NONE" @if($field_rights[$project->id][$role->id][$taskType->id][$field->id] == 'NONE') checked @endif> 0
-                                    </label>
-                                    <label class="btn btn-default @if($field_rights[$project->id][$role->id][$taskType->id][$field->id] == 'READ') active @endif" @if($field_rights[$project->id][$role->id][$taskType->id][$field->id] == 'READ') checked @endif>
-                                      <input type="radio" name="field_right[{{$project->id}}][{{$role->id}}][{{$taskType->id}}][{{$field->id}}]" value="READ"> R
-                                    </label>
-                                    <label class="btn btn-default @if($field_rights[$project->id][$role->id][$taskType->id][$field->id] == 'WRITE') active @endif" @if($field_rights[$project->id][$role->id][$taskType->id][$field->id] == 'WRITE') checked @endif>
-                                      <input type="radio" name="field_right[{{$project->id}}][{{$role->id}}][{{$taskType->id}}][{{$field->id}}]" value="WRITE"> W
-                                    </label>
-                                  </div>
-                                @endif
-                                </td>
-                              @endforeach <!-- role -->
-                          </tr>
-                          @endforeach <!-- field -->
-                        </tbody>
-                        @endforeach <!-- taskType -->
-                      </table>
+                        </table>
+                </div> <!-- <div class="x_title"> -->
+                <div class="x_content" style="display: block;">
+                    @include('admin.right.fields')
+                </div>
+              </div>
                       @endforeach
                       {!! Form::submit('Save', array('class' => 'btn btn-success')) !!}
                     {!! Form::close() !!}
-                  </div>
-                </div>
               </div>
             </div>
-          </div>
-          <script>
-            //$(".active").click();
+@endsection
 
-            function chaingeChildRight(cls, val){
-              //alert('"#'+cls+" :input[value='" + val + "']" + '"');
+@section('js_include')
+          <script>
+          $( document ).ready(function() {
+              console.log( "ready!" );
+              $(".collapse-link").click();
+          });
+          function chaingeChildRight(cls, val){
               $('.'+cls+" :input[value='" + val + "']").click();
             }
-
           </script>
 @endsection
