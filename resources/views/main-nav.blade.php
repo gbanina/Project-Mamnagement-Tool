6db4f87019384e12b9dd855b7884f94e7a0a458b
Goran Banina <gbanina@gmail.com>
2017-06-07 07:25:45 +0200

    removing logo

--- resources/views/main-nav.blade.php
+++ resources/views/main-nav.blade.php
@@ -1,9 +1,3 @@
-            <div class="navbar nav_title" style="border: 0;">
-              <a href="{{ URL::to('/') }}" class="site_title">
-                <img src="{{ URL::to('assets/fav/apple-icon-60x60.png')}}" style="width:30px;height:30px;">
-                <span>eambiosis</span>
-              </a>
-            </div>
             <div class="clearfix"></div>
 
             <!-- menu profile quick info -->
